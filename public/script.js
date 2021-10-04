ab10cf4cb1c4fa53318ad40ab69b09705a5ff26a
AlaynaCarlin <alaynacarlin18@gmail.com>
2021-10-04 09:22:03 -0400

    rough draft

--- public/script.js
+++ public/script.js
@@ -1,9 +1,33 @@
 //* URL
 // ** firebase deploy **
-// const baseURL = "https://calendarific.com/api/v2/holidays?api_key=fb928595c436941d6cbbcd4b408bc64c8300491c&country=US&year=2019";
+const baseURL = "https://calendarific.com/api/v2/holidays?api_key=fb928595c436941d6cbbcd4b408bc64c8300491c&country=US&year=2021";
+const baseURL2 = "https://calendarific.com/api/v2/holidays?api_key=fb928595c436941d6cbbcd4b408bc64c8300491c&country=Mexico&year=2021";
 
-fetch ("https://calendarific.com/api/v2/holidays?api_key=fb928595c436941d6cbbcd4b408bc64c8300491c&country=US&year=2019")
+//*card elements
+let cardTitle = document.getElementById('cardTitle');
+let subtitle = document.getElementById('subtitle');
+let text = document.getElementById('text');
+
+//*button(s)
+let usButton = document.getElementById('USButton');
+
+//*event listener(s)
+usButton.addEventListener('click', FetchHoly)
+
+
+function randomInt (max) {
+ return Math.floor(Math.random() * max);
+}
+
+function FetchHoly (e) {
+    fetch (baseURL)
     .then(res => res.json())
-    .then(function (json) {
-      console.log(json)
-    });
+    .then(json => randoHoliday((json.response.holidays[randomInt(480)])));
+}
+
+function randoHoliday (day){
+  console.log(day);
+  cardTitle.innerText = `Name: ${day.name}`;
+  subtitle.innerText = `Date: ${day.date.iso}`;
+  text.innerText = `description: ${day.description}`;
+}
